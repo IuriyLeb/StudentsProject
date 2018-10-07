8c27f58cf154a8ef0a8d5675c645f425d7366e2c
IuriyLeb <1.u.r.o.n.3@gmail.com>
2018-10-07 18:58:23 +0300

    Bugs from comments have been fixed.

--- Randomizer.py
+++ Randomizer.py
@@ -1,24 +1,21 @@
 import random
 
 
-def randomly(list_of_students):
-    student_dict_reverted = {}
+def random(list_of_students):
+    student_dict = {}
     for elem in list_of_students:
-        student_dict_reverted[elem] = 0
+        student_dict[elem] = 0
     print('Do you want to make your choice?', '\n', 'Press y to do it, n to exit')
     while True:
-        choice2 = input()
-        if choice2 == 'n':
+        choice = input()
+        if choice == 'n':
             break
-        elif choice2 == 'y':
+        elif choice == 'y':
             somestud = random.choice(list_of_students)
-            if student_dict_reverted[somestud] > 0:
+            if student_dict[somestud] > 0:
                 somestud = random.choice(list_of_students)
-                student_dict_reverted[somestud] = 0
-                print(somestud)
-                student_dict_reverted[somestud] += 1
-            else:
-                print(somestud)
-                student_dict_reverted[somestud] += 1
+                student_dict[somestud] = 0
+            print(somestud)
+            student_dict[somestud] += 1
         else:
             print('Incorrect, try again')
