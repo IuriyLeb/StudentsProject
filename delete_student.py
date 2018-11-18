f9c0c3d6f2fd4d9ed2d74c4785710ad83dea4f36
Polinaa19 <pollypavlova98@gmail.com>
2018-11-19 00:27:23 +0300

    Communication with user was removed

--- delete_student.py
+++ delete_student.py
@@ -1,25 +1,10 @@
-def random_st_delete(list_of_student):
-    for i in range(len(list_of_student)):
-        print(i+1, list_of_student[i])
-    print('are all students here? Answer y/n')
-    x = input()
-    if x == 'n':
-        print('who is absent today? print their №')
-        absent_student = [int(i) for i in input().split()]
-        for j in range(len(absent_student)):          
-            list_of_student.remove(list_of_student[j])
-    while len(list_of_student) > 0:
-        print('Choose a random student? y/n')
-        if input() == 'y':
-            q = random.choice(list_of_student)
-            print(q)
-            list_of_student.remove(q)
-
-if __name__ == '__main__':
-    list_of_student = [line.strip().split("\t",1)[1] for line in open("../data_input/list_of_students.txt").readlines()]
-    random_st_delete(list_of_student)
+import random
+'''This module excludes the ability to select the same student twice in one session.'''
 
 
+def delete_random_student(list_of_student):
 
+    random_student = random.choice(list_of_student)
+    list_of_student.remove(random_student)
 
-
+    return random_student, list_of_student
