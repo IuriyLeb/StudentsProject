97db29e6164134e4bd94db418c1e898f2482a653
Polinaa19 <pollypavlova98@gmail.com>
2018-11-19 00:44:01 +0300

    Corrections for working with main module

--- delete_student.py
+++ delete_student.py
@@ -2,9 +2,9 @@ import random
 '''This module excludes the ability to select the same student twice in one session.'''
 
 
-def delete_random_student(list_of_student):
+def delete_random_student(list_of_student: list, student_array=None):
 
     random_student = random.choice(list_of_student)
     list_of_student.remove(random_student)
 
-    return random_student, list_of_student
+    return random_student, student_array
