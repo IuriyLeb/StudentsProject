5aa13ec3a813bf0494904a3c98deaea90d176d16
OOLebedenko <oolebedenko@gmail.com>
2019-02-16 21:09:47 +0300

    Add delete to methods

--- methods/oracle_student.py
+++ methods/oracle_student.py
@@ -1,12 +1,13 @@
 from methods.randomizer import choose_randomly
 from methods.segments import count_student_weight
+from methods.delete_student import delete_random_student
 from methods.utils.mark_student import mark_student
 import sys
 
 
 def oracle(path_to_file_of_student, name_method, student_array=None):
     list_of_student = []
-    choicer = {'randomizer': choose_randomly, 'segments': count_student_weight}
+    choicer = {'randomizer': choose_randomly, 'segments': count_student_weight, 'delete_student': delete_random_student}
     with open(path_to_file_of_student, "r", encoding="utf-8") as students_file:
         for line in students_file:
             student_name = line.strip().split(" ", 1)[1]
