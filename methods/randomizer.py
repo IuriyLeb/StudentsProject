e6ae5ea18b20784d3284a843629ff8d16667066b
OOLebedenko <oolebedenko@gmail.com>
2019-02-16 21:24:31 +0300

    Fix randomizer

--- methods/randomizer.py
+++ methods/randomizer.py
@@ -6,8 +6,8 @@ def choose_randomly(list_of_students: list, student_array=None):
     if not student_array:
         student_array = {}
     for student in list_of_students:
-        student_array[student] = 0
         random_student = random.choice(list_of_students)
+        student_array[random_student] = 0
         if student_array[random_student]:
             random_student = random.choice(list_of_students)
             student_array[random_student] = 0
